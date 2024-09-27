0512f4d17651ba8dd94da1213e6e36ce21656091
Christopher Moreton <christopher.moreton@students.codeinstitute.net>
2024-09-27 18:03:37 +0000

    Added HTML, CSS and JQuery to allow for a welcome pop up message with instructions before game start

--- scripts/card.js
+++ scripts/card.js
@@ -1,11 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
+$(document).ready(function() {
+    // Start Game Button Click Event
+    $("#start-game").click(function() {
+        // Hide the welcome message and show the game content after clicking start
+        $("#welcome-message").fadeOut(500, function() {
+            $("#game-content").fadeIn(500);
+        });
+    });
 
-    function flipCard() {
-
-        this.querySelector('.card-inner').classList.toggle('is-flipped');
-    }
-
-
-    cards.forEach(card => card.addEventListener('click', flipCard));
+    // Card Click Event for Flipping The Cards
+    $(".card").click(function() {
+        $(this).find(".card-inner").toggleClass("is-flipped");
+    });
 });
